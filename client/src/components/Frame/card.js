7fc7a68e462952b6ca0ecbb5604f8f3ce7343490
aoineza <51803728+aoineza@users.noreply.github.com>
2019-08-12 20:29:37 -0700

    Add files via upload

---
+++ client/src/components/Frame/card.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './card.css'
+class Card extends Component{
+    render(){
+        return (
+        <div>
+            <section class='card card_color'>
+                <p>Content</p>
+            </section>
+        </div>
+        )
+    }
+}
+
+export default Card
